3efb4aa03aa012d79fb794f48b2628bf6a08f615
Renato Oliveira <renatorops@gmail.com>
2020-12-18 13:30:11 -0300

    Asset management | loading css

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,16 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        /**
+         * The above order of loaders should be maintained: 'style-loader' comes first and followed by 'css-loader'.
+         * If this convention is not followed, webpack is likely to throw errors.
+         */
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
