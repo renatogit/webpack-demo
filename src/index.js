6596e3adc83395f49e19d15d018bedac0b51075a
Renato Oliveira <renatorops@gmail.com>
2020-12-19 12:59:07 -0300

    Remove the settings for the next step

--- src/index.js
+++ src/index.js
@@ -1,37 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from 'assets/icon.png';
-import Data from 'mock/data.xml';
-import Notes from 'mock/data.csv';
-import toml from 'mock/data.toml';
-import yaml from 'mock/data.yaml';
-import json from 'mock/data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div
-  const myIcon = new Image();
-
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
 
   return element;
 }
